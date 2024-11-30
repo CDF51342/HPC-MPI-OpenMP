a7cd7d56e1b5107d3507bf175c1c1d8ac710e534
Raúl Ágreda <100451269@alumnos.uc3m.es>
2024-11-30 20:33:52 +0100

    Added all the failed versions to the summary

--- runTest.sh
+++ runTest.sh
@@ -6,6 +6,8 @@
 #SBATCH --time=01:00:00
 #SBATCH --partition=compute
 
+versions=("OpenMP" "MPI" "MPI+OpenMP")
+
 read -p "Do you want to compile the projects? (y/n): " compile_choice
 if [ "$compile_choice" == "y" ]; then
     printf "Compiling projects...\n"
@@ -25,7 +27,7 @@ mv ./out* ./OutputCheck
 mpirun -np 1 ./OpenMP/build/contrast
 # check the difference between the sequential and OpenMP versions
 check_output_difference() {
-    version=$1
+    version=${versions[$1]}
     passed=true
     for file in ./out*; do
         diff "$file" "./OutputCheck/$(basename "$file")" > /dev/null
@@ -36,23 +38,23 @@ check_output_difference() {
     done
     if [ "$passed" = true ]; then
         echo -e "\e[32m✔\e[0m $version version passed!"
-        passed_versions+=("$version")
+        passed_versions[$1]="1"
     fi
 }
 
-passed_versions=()
+passed_versions=("0" "0" "0")
 
-check_output_difference "OpenMP"
+check_output_difference 0
 
 # Execute MPI version, and save the output
 mpirun -np 4 ./MPI/build/contrast
 # check the difference between the sequential and MPI versions
-check_output_difference "MPI"
+check_output_difference 1
 
 # Execute MPI+OpenMP version, and save the output
 mpirun -np 4 ./MPI+OpenMP/build/contrast
 # check the difference between the sequential and MPI+OpenMP versions
-check_output_difference "MPI+OpenMP"
+check_output_difference 2
 
 rm ./out*
 
@@ -60,7 +62,13 @@ rm ./out*
 rm -r OutputCheck
 
 # Summary of passed versions
+echo "==================================="
 echo "Summary of passed versions:"
-for version in "${passed_versions[@]}"; do
-    echo -e "\e[32m✔\e[0m $version version passed!"
+for i in "${!versions[@]}"; do
+    version=${versions[$i]}
+    if [ "${passed_versions[$i]}" == "0" ]; then
+        echo -e "\e[31m✘\e[0m Error: $version version"
+    else
+        echo -e "\e[32m✔\e[0m $version version passed!"
+    fi
 done
