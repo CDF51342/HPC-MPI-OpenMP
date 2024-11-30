0b4ea882c9c657ed5ef4b8d398df881374742bc6
Carlos <100451342@alumnos.uc3m.es>
2024-11-30 11:45:16 +0100

    Create sol images

--- generateInputFiles.sh
+++ generateInputFiles.sh
@@ -1,3 +1,15 @@
-mkdir TestFiles
+mkdir TestFiles images_seq
+
+echo "Converting images..."
 convert highres.jpg TestFiles/in.ppm
 convert highres.jpg TestFiles/in.pgm
+echo "Finish convertion!"
+
+echo "Creating images solution..."
+make contrast_seq
+./contrast_seq
+mv out.pgm images_seq/out.pgm
+mv out_hsl.ppm images_seq/out_hsl.ppm
+mv out_yuv.ppm images_seq/out_yuv.ppm
+rm ./contrast_seq
+echo "Done images_seq/ !"
